f9eff9ee127380660827744519ab11bb92f06896
kuyu310 <limeng@limeng-mac.lan>
2017-03-01 23:36:42 +0800

    解决桥接文件的路径问题

--- E-housekeeper/E-housekeeper-Bridging-Header.h
+++ E-housekeeper/E-housekeeper-Bridging-Header.h
@@ -8,8 +8,8 @@
 
 #ifndef E_housekeeper_Bridging_Header_h
 #define E_housekeeper_Bridging_Header_h
-#import <MJExtension/MJExtension.h>
-#import "SVProgressHUD.h"
-#import "OnboardingViewController.h"
+#import "MJExtension/MJExtension.h"
+#import "SVProgressHUD/SVProgressHUD.h"
+#import "Onboard/OnboardingViewController.h"
 
 #endif /* E_housekeeper_Bridging_Header_h */
